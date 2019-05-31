c097d706a9e1952340259d415fa00c322ed17c54
Thomas Papp <pappmthomas@gmail.com>
2019-05-31 13:35:23 -0400

    Added support for header tags, i tag, u tag, color tag, center tag, lists tags, and img tag

--- src/nodes.js
+++ src/nodes.js
@@ -8,6 +8,71 @@ const nodes = [
         tag: "b",
         html_open: "<strong>",
         html_close: "</strong>"
+    },
+    {
+        tag: "i",
+        html_open: "<i>",
+        html_close: "</i>"
+    },
+    {
+        tag: "u",
+        html_open: "<u>",
+        html_close: "</u>"
+    },
+    {
+        tag: "h1",
+        html_open: "<h1>",
+        html_close: "</h1>"
+    },
+    {
+        tag: "h2",
+        html_open: "<h2>",
+        html_close: "</h2>"
+    },
+    {
+        tag: "h3",
+        html_open: "<h3>",
+        html_close: "</h3>"
+    },
+    {
+        tag: "h4",
+        html_open: "<h4>",
+        html_close: "</h4>"
+    },
+    {
+        tag: "h5",
+        html_open: "<h5>",
+        html_close: "</h5>"
+    },
+    {
+        tag: "h6",
+        html_open: "<h6>",
+        html_close: "</h6>"
+    },
+    {
+        tag: "color",
+        html_open: attr => `<span style="color:${attr || "black"};">`,
+        html_close: "</span>"
+    },
+    {
+        tag: "center",
+        html_open: `<span style="text-align: center">`,
+        html_close: "</span>"
+    },
+    {
+        tag: "list",
+        html_open: "<ul>",
+        html_close: "</ul>"
+    },
+    {
+        tag: "olist",
+        html_open: "<ol>",
+        html_close: "</ol>"
+    },
+    {
+        tag: "img",
+        html_open: attr => `<img src="${attr || "#"}" alt="${attr || "#"}" />`,
+        html_close: ""
     }
 ];
 
